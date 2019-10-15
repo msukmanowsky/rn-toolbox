b636974b06e9e938a0aca3e29f1da8a72d486a48
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2019-10-14 23:29:13 -0400

    Update SlideAndFadeIn docs

--- src/animations/SlideAndFadeIn.tsx
+++ src/animations/SlideAndFadeIn.tsx
@@ -18,6 +18,27 @@ interface State {
   outputRange: [number, number];
 }
 
+/**
+ * Wrapper View that uses react-native-reanimated to slide and fade in a View
+ * and it's subviews.
+ *
+ * ```typescript
+ * <SlideAndFadeIn direction="up">
+ *   <Text>Hello</Text>
+ * </SlideAndFadeIn>
+ * ```
+ *
+ * If you don't want the animation to run immediately, you can pass
+ * autoRun={false} and then use a ref to trigger animation.
+ * ```typescript
+ * const slideAndFadeIn = useRef(null);
+ * <SlideAndFadeIn ref={slideAndFadeIn}>
+ *   <Text>Hello</Text>
+ * </SlideAndFadeIn>
+ * // Elsewhere in your code
+ * slideAndFadeIn.current.animate();
+ * ```
+ */
 export default class SlideAndFadeIn extends React.Component<
   SlideAndFadeInProps,
   State
